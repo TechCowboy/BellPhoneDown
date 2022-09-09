c6c4ba4d50bdb274f23b7d52755439112f352fd2
Norman Davie <ecompservices@gmail.com>
2022-09-09 11:21:49 -0300

    Update bellphonedown.py
    Added lots of comments
    Added other browsers to the mix, but only tested Chrome, Edge and IE
    Chrome can be run headless
    Use the generic URL http://home to get to the router
    Added some error checking

--- bellphonedown.py
+++ bellphonedown.py
@@ -20,8 +20,13 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import IEDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
 import time
 from bs4 import BeautifulSoup
 import re
@@ -73,20 +78,15 @@ else:
 failure = "DOWN"
 success = "UP"
  
-# url to the Bell router
-
-global_dynamicUrl = "http://192.168.2.1"
-
 
 options = Options()
-# we control chrome to get the webpage as a headless browser
 
-#options.headless = True
-#options.add_argument("--window-size=1920,1080")
-#options.add_argument("start-maximized")
-#options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
 
 last_status = ""
+
+# Bell has an annoying intro that requires us to wait
+# before the data is available. Number of seconds to wait.
+delay_reading_page = 10
 
 # frequency to check in minutes
 minutes = 15
@@ -94,110 +94,143 @@ minutes = 15
 # on the first time, we want to send an email
 # if there is a failure, otherwise send an
 # email whenever a service status changes
-
 first_time = True
+
+# I've only tested Chrome, Edge, and Internet Explorer, however, other browsers are supported,
+# but have not been tested
+
+# Chrome
+
+# optionally we can control chrome as a headless browser
+options.headless = True
+options.add_argument("--window-size=1920,1080")
+options.add_argument("start-maximized")
+options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-while(True):
 
-    # open the page and evaluate the javascript as well
-    driver.execute_script("window.open('http://192.168.2.1', '_self')")
+# Chromium
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
-    # the page has a useless self promotion when starting, so we need
-    # to wait for the page to completely load
-    print("Waiting 10 seconds for page to load...")
-    time.sleep(10)
-    
-    print("Getting page...")
-    # the page should be here now
-    pageSource = driver.page_source
+# Brave
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()))
 
-    # parse the page and find the 3 services
-    soup = BeautifulSoup(pageSource, "html.parser")
-    
-    data_internet = soup.find("div", id="internetContainer")
-    data_tv       = soup.find("div", id="fibeTvContainer")
-    data_voice    = soup.find("div", id="fibeVoiceContainer")
+# FireFox
+#driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
-    # use this search pattern to find out the current status
-    pattern = r'status="(\w*)"'
+# Edge
+#driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
 
-    # clear any existing status
-    internet = None
-    tv       = None
-    phone    = None
+# Internet Explorer
+#driver = webdriver.Ie(service=Service(IEDriverManager().install()))
 
-    # find out the status of each service
-    status = re.search(pattern, str(data_internet))
-    if status != None:
-        internet = status.groups()[0].upper()
-        print("Internet: " + status.groups()[0])
-        no_internet = internet != success
-    else:
-        print("No internet status")
-        no_internet = True
+try:
+    while(True):
 
-    status = re.search(pattern, str(data_tv))
-    if status != None:
-        tv = status.groups()[0].upper()
-        print("TV: " + status.groups()[0])
-    else:
-        print("No tv status")
+        # open the page and evaluate the javascript as well
+        driver.execute_script("window.open('http://home', '_self')")
+
+        # the page has a useless self promotion when starting, so we need
+        # to wait for the page to completely load
+        print(f"Waiting {delay_reading_page} seconds for page to load...")
+        time.sleep(delay_reading_page)
         
+        # the page should be here now
+        print("Getting page...")
+        pageSource = driver.page_source
 
-    status = re.search(pattern, str(data_voice))
-    if status != None:
-        phone = status.groups()[0].upper()
-        print("Phone: " + status.groups()[0])
-    else:
-        print("No phone status")
-
-
-    # no point in attempting to email if there is no internet
-    if no_internet:
-        print(f"Can't send or can't determine current internet state")
-    else:
-        status = [internet, tv, phone]
-
-        if first_time:
-            last_status = status
-            if failure in status:
-                last_status = "" # force an email to be sent
+        # parse the page and find the 3 services
+        soup = BeautifulSoup(pageSource, "html.parser")
         
-        #print(status)
-        
-        if last_status != status:
-            last_status = status
-            print("Sending Email!")
+        data_internet = soup.find("div", id="internetContainer")
+        data_tv       = soup.find("div", id="fibeTvContainer")
+        data_voice    = soup.find("div", id="fibeVoiceContainer")
+
+        # use this search pattern to find out the current status
+        pattern = r'status="(\w*)"'
+
+        # clear any existing status
+        internet = None
+        tv       = None
+        phone    = None
+
+        # find out the status of each service
+        status = re.search(pattern, str(data_internet))
+        if status != None:
+            internet = status.groups()[0].upper()
+            print("Internet: " + status.groups()[0])
+            no_internet = internet != success
+        else:
+            print("No internet status")
+            no_internet = True
+
+        status = re.search(pattern, str(data_tv))
+        if status != None:
+            tv = status.groups()[0].upper()
+            print("      TV: " + status.groups()[0])
+        else:
+            print("No tv status")
             
-            subject = 'Bell Service Status Change!'
-            body = "\nInternet: " + internet + "\nTV:       " + tv + "\nPhone:    " + phone + "\n"
 
-            email_text = """\
-From: %s
-To: %s
-Subject: %s
+        status = re.search(pattern, str(data_voice))
+        if status != None:
+            phone = status.groups()[0].upper()
+            print("   Phone: " + status.groups()[0])
+        else:
+            print("No phone status")
 
-%s
-""" % (sender_email, ", " +receiver_email, subject, body)
+
+        # no point in attempting to email if there is no internet
+        if no_internet:
+            print(f"Can't connect or can't determine current internet state")
+        else:
+            status = [internet, tv, phone]
+
+            if first_time:
+                last_status = status
+                if failure in status:
+                    # force an email to be sent if there is a failure on the
+                    # first status check
+                    last_status = "" 
             
-            try:
-                server = smtplib.SMTP_SSL(smtp_server, port)
-                server.ehlo()
-                server.login(sender_email, sender_password)
-                server.sendmail(sender_email, receiver_email, email_text)
-                server.close()
+            #print(status)
+            
+            # Everytime the status changes, send an email
+            if last_status != status:
+                last_status = status
+                print("Sending Email!")
+                
+                subject = 'Bell Service Status Change!'
+                body = "\nInternet: " + internet + "\nTV:       " + tv + "\nPhone:    " + phone + "\n"
 
-                print('Email sent!')
-            except Exception as e:
-                print(f'Something went wrong sending email\n{str(e)}')
+                email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
 
-    remaining = 60 * minutes
-    while remaining > 0:
-        print(f"Next check in {remaining} seconds  ...\r", end='')
-        time.sleep(1)
-        remaining -= 1
-        
+    %s
+    """ % (sender_email, ", " +receiver_email, subject, body)
+                
+                try:
+                    server = smtplib.SMTP_SSL(smtp_server, port)
+                    server.ehlo()
+                    server.login(sender_email, sender_password)
+                    server.sendmail(sender_email, receiver_email, email_text)
+                    server.close()
+
+                    print('Email sent!')
+                except Exception as e:
+                    print(f'Something went wrong sending email\n{str(e)}')
+
+        # wait for a while and then check again
+        remaining = 60 * minutes
+        while remaining > 0:
+            print(f"Next check in {remaining} seconds  ...\r", end='')
+            time.sleep(1)
+            remaining -= 1
+except Exception as e:
+    print("Error occured: "+ str(e))
+    
 driver.close()
 driver.quit()
